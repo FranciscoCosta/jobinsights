43119e31003788ee744d294a821822b07803c06a
Xico <francisco100eg@gmail.com>
2023-04-27 15:44:56 -0300

    Feat: filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -29,6 +29,9 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
+
+    filtered_industry = [job for job in jobs if job["industry"] == industry]
+    return filtered_industry
     """Filters a list of jobs by industry
 
     Parameters
